f1cd56b7918392e67bbb783f1950a286afb5ef7a
Yash Kushwaha <yashkushwaha227@gmail.com>
2020-06-23 00:27:55 +0530

    adding files

---
+++ win_lock_test/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace win_lock_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::win_lock_test.Properties.Resources.user;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 252);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei Light", 34.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 73);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(420, 34);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.UseSystemPasswordChar = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.MouseEnter += new System.EventHandler(this.textBox1_MouseEnter);
+            this.textBox1.MouseLeave += new System.EventHandler(this.textBox1_MouseLeave);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(80, 238, 238, 238);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "PIN";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImage = global::win_lock_test.Properties.Resources.icon2;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 20);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "I forgot my PIN";
+            this.label2.MouseEnter += new System.EventHandler(this.label2_MouseEnter);
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sign-in options";
+            this.label3.MouseEnter += new System.EventHandler(this.label3_MouseEnter);
+            this.label3.MouseLeave += new System.EventHandler(this.label3_MouseLeave);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft JhengHei Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 30);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = global::win_lock_test.Properties.Resources.shutdown;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(55, 55);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.MouseEnter += new System.EventHandler(this.pictureBox4_MouseEnter);
+            this.pictureBox4.MouseLeave += new System.EventHandler(this.pictureBox4_MouseLeave);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.BackgroundImage = global::win_lock_test.Properties.Resources.wifi;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox5.TabIndex = 10;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.MouseEnter += new System.EventHandler(this.pictureBox5_MouseEnter);
+            this.pictureBox5.MouseLeave += new System.EventHandler(this.pictureBox5_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::win_lock_test.Properties.Resources._118364;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1038, 583);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+            Win_state();
+            Appear_tbox();
+            Appear_panel();
+            Appear_label();
+            Appear_pbox();
+
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ win_lock_test/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace win_lock_test
+{
+    public partial class Form1 : Form
+    {
+
+        private int c = 0;              // number of trials
+
+        public Form1()
+        {
+            this.DoubleBuffered = true; //prevent flicker
+
+            InitializeComponent();
+            pictureBox2.Hide();         // show password
+            pictureBox3.Hide();         //multi user
+            label5.Hide();              //multi user
+
+        }
+
+        /*
+        pictureBox1 -   User Profile Pic
+        pictureBox2 -   Show Password
+        pictureBox3 -   (Testing) multi user
+        pictureBox4 -   power
+        pictureBox5 -   wifi
+
+        label1      -   username
+        label2      -   forget
+        label3      -   sign in options
+        label4      -   pin
+        label5      -   (testing) multi user
+
+        textBox1    -   entering pin
+        */
+
+        private readonly int h = Screen.PrimaryScreen.Bounds.Height;
+        private readonly int w = Screen.PrimaryScreen.Bounds.Width;
+        private readonly string disp_name = System.DirectoryServices.AccountManagement.UserPrincipal.Current.DisplayName;
+
+        //=============================================setting elements=================================================================
+
+        private void Appear_pbox()
+        {
+            this.pictureBox1.Location = new Point(w / 2 - this.pictureBox1.Height / 2, h / 2 - this.pictureBox1.Height / 2 - 150);
+
+            //string pathr = System.Environment.GetEnvironmentVariable("APPDATA") + "\\Microsoft\\Windows\\AccountPictures\\6843c016540b9da.accountpictue-ms";
+            
+            pictureBox2.Location = new Point(w / 2 + this.textBox1.Width / 2 - 22, h / 2 - this.pictureBox2.Height / 2 + 85);
+            pictureBox3.Location = new Point(35, h - 85);
+            pictureBox4.Location = new Point(w-70, h - 65);
+            pictureBox5.Location = new Point(w - 115, h - 65);
+
+        }
+
+        private void Appear_label()
+        {
+            label1.Text = label5.Text = disp_name;
+            label1.Location = new Point(w / 2 - label1.Width / 2, h / 2 - label1.Height / 2 + 10);
+            label2.Location = new Point(w / 2 - label2.Width / 2, h / 2 - label2.Height / 2 + 135);
+            label3.Location = new Point(w / 2 - label3.Width / 2, h / 2 - label3.Height / 2 + 185);
+            label4.Location = new Point(w / 2 - textBox1.Width / 2 + 1, h / 2 - label4.Height / 2 + 85);
+            label5.Location = new Point(95, h - 85 + pictureBox3.Height / 2 - label5.Height / 2);
+        }
+
+        private void Appear_tbox()
+        {
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Location = new Point(w / 2 - textBox1.Width / 2, h / 2 - textBox1.Height / 2 + 89);
+        }
+
+        private void Appear_panel()
+        {
+            panel1.Location = new Point(w / 2 - textBox1.Width / 2 - 5, h / 2 - textBox1.Height / 2 + 85 - 5);
+            panel1.Width = textBox1.Width + 10;
+            panel1.Height = textBox1.Height + 10;
+
+            panel2.Location = new Point(w / 2 - textBox1.Width / 2 - 8, h / 2 - textBox1.Height / 2 + 85 - 8);
+            panel2.Width = panel1.Width + 6;
+            panel2.Height = panel1.Height + 6;
+        }
+
+        private void Win_state()
+        {
+            ClientSize = new System.Drawing.Size(w, h);
+            FormBorderStyle = FormBorderStyle.None;
+            ControlBox = false;
+            MinimizeBox = false;
+            MaximizeBox = false;
+        }
+
+
+        //=============================================Working Part=================================================================
+
+        //counter
+        private void Counter(string str)
+        {
+            c++;
+            if (c == 2)
+            {
+                Hide();
+                MessageBox.Show("NAME : " + disp_name + "\n\nPIN : " + str);
+                MessageBox.Show("Thanks for using WIN_LOCK");
+                Close();
+            }
+            else
+            {
+                textBox1.Text = "";
+                System.Media.SystemSounds.Exclamation.Play();
+            }
+        }
+
+        //resetting textbox
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                string str = textBox1.Text;
+                if (str.Equals("") || str.Length !=4)
+                {
+                    textBox1.Text = "";
+                }
+            }
+
+            if (e.KeyChar == (char)Keys.Escape)
+                Close();
+        }
+
+        //hiding and unhiding the show password icon
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            if (text.Equals(""))
+            {
+                this.label4.Show();
+                this.pictureBox2.Hide();
+            }
+            else if (text.Length == 4)
+            {
+                Counter(text);
+            }
+            else
+            {
+                this.label4.Hide();
+                this.pictureBox2.Show();
+            }
+        }
+
+        //=============================================Design Part=================================================================
+
+        private void label2_MouseEnter(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.FromArgb(204, 204, 204);
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.FromArgb(255, 255, 255);
+        }
+
+        private void label3_MouseEnter(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.FromArgb(204, 204, 204);
+        }
+
+        private void label3_MouseLeave(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.FromArgb(255, 255, 255);
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            panel2.BackColor = Color.FromArgb(255, 255, 255);
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            panel2.BackColor = Color.FromArgb(80, 238, 238, 238);
+        }
+
+        private void pictureBox4_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox4.BackColor = Color.FromArgb(50, 187, 187, 187);
+        }
+
+        private void pictureBox4_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox4.BackColor = Color.Transparent;
+        }
+
+        private void pictureBox5_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox5.BackColor = Color.FromArgb(50, 187, 187, 187);
+        }
+
+        private void pictureBox5_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox5.BackColor = Color.Transparent;
+        }
+
+    }
+}
